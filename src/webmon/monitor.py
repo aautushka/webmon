d332f58353372c5ded0b9dfcbfd61138d810e0ee
Anton Aŭtuška <antoninus.liberalis@gmail.com>
2023-07-05 10:10:25 +0300

    introduce client timeout

--- src/webmon/monitor.py
+++ src/webmon/monitor.py
@@ -13,40 +13,45 @@ from webmon import util
 # * regex failure
 
 
-async def fetch_url(session: aiohttp.ClientSession, request: dict) -> dict:
+async def fetch_url(request: dict) -> dict:
     try:
-        async with session.get(request["url"]) as response:
-            return {**request, "status": "completed", "code": response.status}
+        seconds = request.get("schedule", 300)
+        timeout = aiohttp.ClientTimeout(total=seconds)
+
+        async with aiohttp.ClientSession(timeout=timeout) as session:
+            async with session.get(request["url"]) as response:
+                return {**request, "status": "completed", "code": response.status}
+    except asyncio.exceptions.TimeoutError:
+        return {**request, "status": "client timeout"}
     except Exception as e:
-        print(e)
+        print(f"exception {e} of type {type(e)}")
         return {**request, "status": "exception"}
 
 
 async def run_async(source, sink) -> None:
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        first_received = None
-        while True:
-            batch = None
-            if not source.empty():
-                batch = source.get()
+    tasks = []
+    first_received = None
+    while True:
+        batch = None
+        if not source.empty():
+            batch = source.get()
 
-            if batch:
-                # for debugging
-                if not first_received:
-                    first_received = util.now()
+        if batch:
+            # for debugging
+            if not first_received:
+                first_received = util.now()
 
-                tasks += [asyncio.create_task(fetch_url(session, x)) for x in batch]
+            tasks += [asyncio.create_task(fetch_url(x)) for x in batch]
 
-            if tasks:
-                done, inprogress = await asyncio.wait(tasks, timeout=0.1)
-                tasks = [x for x in inprogress]
+        if tasks:
+            done, inprogress = await asyncio.wait(tasks, timeout=0.1)
+            tasks = [x for x in inprogress]
 
-                if done:
-                    results = [x.result() for x in done]
-                    sink.put(results)
+            if done:
+                results = [x.result() for x in done]
+                sink.put(results)
 
-        await asyncio.sleep(0.5)
+    await asyncio.sleep(0.5)
 
 
 def monitor(source, sink) -> None:
--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -2,25 +2,68 @@ from webmon.monitor import fetch_url
 import aiohttp
 import asyncio
 import pytest
+import time
 
 from aiohttp import web
 
-async def hello(request):
-        return web.Response(text="hello")
 
-async def fetch_async(request):
-    async with aiohttp.ClientSession() as session:
-        return await asyncio.gather(fetch_url(session, request))
+async def http200(request):
+    return web.Response(text="success")
+
+
+async def http500(request):
+    raise Exception("server error")
+
+
+async def sleep(request):
+    ms = request.query.get("ms", 0)
+    time.sleep(int(ms) / 1000)
+    return web.Response(text="slept")
+
+
+async def start(aiohttp_server):
+    app = web.Application()
+    for f in [http200, http500, sleep]:
+        app.router.add_get(f"/{f.__name__}", f)
+
+    return await aiohttp_server(app)
+
 
 @pytest.mark.asyncio
-async def test_http(aiohttp_server):
-    app = web.Application()
-    app.router.add_get('/hello', hello)
-    server = await aiohttp_server(app)
+async def test_http_200(aiohttp_server):
+    server = await start(aiohttp_server)
 
-    resp = await fetch_async({"url": server.make_url('hello'), "schedule": 1})
+    url = server.make_url("http200")
+    resp = await fetch_url({"url": url})
 
-    assert 1 == len(resp)
-    assert 200 == resp[0]['code']
-    assert 'completed' == resp[0]['status']
+    assert {"url": url, "code": 200, "status": "completed"} == resp
 
+
+@pytest.mark.asyncio
+async def test_http_500(aiohttp_server):
+    server = await start(aiohttp_server)
+
+    url = server.make_url("http500")
+    resp = await fetch_url({"url": url})
+
+    assert {"url": url, "code": 500, "status": "completed"} == resp
+
+
+@pytest.mark.asyncio
+async def test_http_404(aiohttp_server):
+    server = await start(aiohttp_server)
+
+    url = server.make_url("notfound")
+    resp = await fetch_url({"url": url})
+
+    assert {"url": url, "code": 404, "status": "completed"} == resp
+
+
+@pytest.mark.asyncio
+async def test_client_timeout(aiohttp_server):
+    server = await start(aiohttp_server)
+
+    url = server.make_url("sleep?ms=1500")
+    resp = await fetch_url({"url": url, "schedule": 1})
+
+    assert {"url": url, "status": "client timeout", "schedule": 1} == resp
